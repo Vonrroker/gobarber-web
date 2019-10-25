8ff1d47625c1f606c56cf4c4b94807080d8374c2
Vonrroker <dayhamsoares@gmail.com>
2019-10-25 19:01:28 -0300

    style height fix

--- src/styles/global.js
+++ src/styles/global.js
@@ -15,8 +15,13 @@ export default createGlobalStyle`
     outline:0;
   }
 
+  html, body, #root {
+    height: 100%;
+  }
+
   body {
     -webkit-font-smoothing: antialiased;
+    height: 100%;
   }
 
   body, input, button {
